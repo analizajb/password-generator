dab828244a1e4652050f63d3fb40214f9fd3ede9
analizajb <96861913+analizajb@users.noreply.github.com>
2022-06-06 11:10:48 -0700

    inputting more specific validation for promptLength = successful

--- Develop/script.js
+++ Develop/script.js
@@ -7,16 +7,28 @@ var charSet = {
   lowerCase: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
   upperCase: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
   numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9],
-  ascii: ['!','"','#','$','%','&','(',')','*','+']
+  symbols: ['!','"','#','$','%','&','(',')','*','+']
 };
 
 function generatePassword() {
-  var promptUser = window.prompt('How many characters would you like your password to include?');
+  promptLength = window.prompt('How many characters would you like your password to include?');
+  console.log("You've chosen " + promptLength + " characters.");
     // validate prompt is answered
-    if (promptUser === "" || promptUser === null) {
+    if (!promptLength) {
       window.alert("Please enter a number between 8 and 128!");
       return generatePassword();
+    } else if (promptLength < 8 || promptLength > 128) {
+      window.alert("Please enter a number between 8 and 128!");
+      console.log("You've chosen " + promptLength + " characters.");
+      return generatePassword();
+    } else {
+      
     }
+  
+  // var lowerCase = window.confirm('Would you like to include lowercase letters?');
+  // if (lowerCase.confirm === true)
+ 
+
   
   // 1. PROMPT USER
   //    - length of password: 8-128 characters
